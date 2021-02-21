16062be369e21f6dfbb652b4ba081309dd3e9a74
Dima Kuriptya <dima343782@gmail.com>
2021-02-21 15:46:25 +0200

    i try guess models but not sure its right

--- src/route/models.py
+++ src/route/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# class Cities(models.Model):
+
+class Cities(models.Model):
+    city = models.CharField('Город', max_length=50)
+
+
+class Trains(models.Model):
+    train_code = models.CharField('Код поезда', max_length=20, unique=True)
+    train_start = models.ForeignKey(Cities, related_name="Место_старта", on_delete=models.CASCADE)
+    train_end = models.ForeignKey(Cities, related_name='Конечная_остановка', on_delete=models.CASCADE)
+    trip_time = models.IntegerField('Время в пути')
+
+
+class Routes(models.Model):
+    route_name = models.CharField('имя маршрута', max_length=30)
+    route_start = models.ForeignKey(Cities, related_name='Начало_маршрута', on_delete=models.CASCADE)
+    route_end = models.ForeignKey(Cities, related_name='Конец_маршрута', on_delete=models.CASCADE)
